0059d3021fe1285855d3a58dcc3cb0247f27b72f
hiram43 <hiram4043@hotmail.com>
2020-07-12 22:41:48 -0500

    Vanilla Js

--- Ecma.js
+++ Ecma.js
@@ -1,16 +1,26 @@
-// JavaScript source code
+// Vanilla JavaScript
 //----------------------------------------------Creating a NavBar--------------------------------------------------------------------------------------------------------------------------------
 let tag = document.createElement("div");
 tag.id = "NavId";
 //Creating Nav Elements
 let NavBarOb1 = document.createElement("a");
+NavBarOb1.href = "https://www.google.com/"
+NavBarOb1.target ="_blank"
 let NavBarOb2 = document.createElement("a");
+NavBarOb2.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+NavBarOb2.target ="_blank"
 let NavBarOb3 = document.createElement("a");
+NavBarOb3.href = "https://www.youtube.com/watch?v=t8FHSNIc3wI"
+NavBarOb3.target ="_blank"
 let NavBarOb4 = document.createElement("a");
+NavBarOb4.target ="_blank"
+NavBarOb4.href ="https://www.youtube.com/watch?v=12DPT3uutdU"
 let ImgNavBar = document.createElement("img");
+ImgNavBar.alt = "TopImg"
+ImgNavBar.href ="https://www.youtube.com/watch?v=aKD86dc4U0c"
 NavBarOb1.innerHTML = "News";
-NavBarOb2.innerHTML = "Contact";
-NavBarOb3.innerHTML = "About";
+NavBarOb2.innerHTML = "Nudes";
+NavBarOb3.innerHTML = "Secret Song";
 NavBarOb4.innerHTML = "Info";
 ImgNavBar.src = "https://upload.wikimedia.org/wikipedia/commons/d/df/Img_logo_blue.jpg";
 ImgNavBar.style.width = "100px"
@@ -42,5 +52,19 @@ element.appendChild(TabBody);
 BodyImg.alt ="Img-Boddy"
 BodyImg.src ="https://lh4.googleusercontent.com/1ClelnhE69INfK-vVBtKLCt4ypoo0CIeWV9TPKNmqNny1rUvRaQVXeJduqAmKmruOBxUBEqVVIcW-78LTmjMHOSVWm1jeTIPo9vDhcMcBvCS9HsxxIORtf3AgOgJlaEblwfXgMFp"
 TabBody.appendChild(BodyImg);
-BodyImg.style.width = "1517.0px"
-BodyImg.style.overflow ="hidden"
+BodyImg.style.width = "78%"
+BodyImg.style.marginLeft = "225px"
+BodyImg.style.marginTop ="100px"
+
+//-----------------------------------------------End Body Content----------------------------------------------------------------------------------
+
+
+//-----------------------------------------------Footer--------------------------------------------------------------------------------------------------
+let Footer = document.createElement("footer");
+let FooterImg = document.createElement("img");
+FooterImg.src ="https://juliancastiblanco.com/web/wp-content/uploads/2017/05/registrate.png"
+element.appendChild(Footer);
+element.appendChild(FooterImg);
+FooterImg.style.width = "25%";
+FooterImg.style.marginTop = "25px"
+FooterImg.style.marginLeft ="40%"
